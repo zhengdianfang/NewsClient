3acb2b9d4132f50f66237d9ef3b0818cdd3e843d
Dianfang Zheng <dfgzheng@thoughworks.com>
2019-08-19 21:19:30 +0800

    add launcher activity

---
+++ app/src/main/java/com/zhengdianfang/newsclientdemo/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.zhengdianfang.newsclientdemo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zhengdianfang.newsclientdemo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
